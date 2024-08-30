ad229de28e559b4337963f092eb29459058c4636
Dirk Ostertag <d@dostertag.de>
2024-08-30 21:45:24 +0200

    refactoring project structure

--- getDependencies.sh
+++ getDependencies.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+DEPS_DIR=_deps
+
 GOOGLE_TEST_VERSION="1.12.1"
 GOOGLE_TEST_ZIP=release-${GOOGLE_TEST_VERSION}.zip
-GOOGLE_TEST_DIR=googletest
+GOOGLE_TEST_DIR=${DEPS_DIR}/googletest
 
 function getGoogleTests(){
+    echo "downloading googletest"
     if [ ! -d "$GOOGLE_TEST_DIR" ]; then
         wget https://github.com/google/googletest/archive/$GOOGLE_TEST_ZIP
         unzip $GOOGLE_TEST_ZIP
@@ -14,5 +17,9 @@ function getGoogleTests(){
         echo "$GOOGLE_TEST_DIR already present!"
     fi
 }
+
+if [ ! -d $DEPS_DIR ]; then
+    mkdir $DEPS_DIR
+fi
 
 getGoogleTests
